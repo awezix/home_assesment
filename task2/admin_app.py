1295742bd5bc6c789335cbbc712bc55d96560ee6
arsh ansari <mdarshansari3426@gmail.com>
2025-12-07 00:18:16 +0530

    updated admin_app.py

--- task2/admin_app.py
+++ task2/admin_app.py
@@ -1,10 +1,20 @@
 import streamlit as st
 import pandas as pd
+import os
 
-st.title("Admin dashboard")
+
+if not os.path.exists("storage"):
+    os.makedirs("storage")
+
 DATA_FILE = "storage/data.csv"
+# create csv if missing
+if not os.path.exists(DATA_FILE):
+    df = pd.DataFrame(columns=["rating", "review", "ai_response", "summary", "actions", "date","time"])
+    df.to_csv(DATA_FILE, index=False)
+
 df=pd.read_csv(DATA_FILE)
 
+st.title("Admin dashboard")
 if len(df)==0:
     st.warning("No data found")
 else:
